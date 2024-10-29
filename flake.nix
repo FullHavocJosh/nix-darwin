01c9a5c4097f4d4ab03ae12cdeafe606fa5b4b58
havoc <josh@rollet.family>
2024-10-29 08:18:57 -0400

    created _work config

--- flake.nix
+++ flake.nix
@@ -25,13 +25,6 @@
       fonts.packages = [
         (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
       ];
-
-      system.activationScripts.script.text = ''
-        echo "Stowing dotfiles..."
-        cd /Users/havoc/nix-darwin || { echo "Failed to cd into ~/nix-darwin/dotfiles"; exit 1; }
-        echo "Stowing $dir..."
-        /run/current-system/sw/bin/stow -R . || { echo "Failed to stow ."; exit 1; }
-      '';
     };
 
     # List packages installed in system profile. To search by name, run:
@@ -138,7 +131,7 @@
       };
     };
 
-    macosConfigModule = { pkgs, config, ... }: {
+    macosConfigModule_personal = { pkgs, config, ... }: {
       system.activationScripts.applications.text = let
         env = pkgs.buildEnv {
           name = "system-applications";
@@ -155,6 +148,13 @@
           echo "copying $src" >&2
           ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
         done
+      '';
+
+      system.activationScripts.script.text = ''
+        echo "Stowing dotfiles..."
+        cd /Users/havoc/nix-darwin || { echo "Failed to cd into ~/nix-darwin/dotfiles"; exit 1; }
+        echo "Stowing $dir..."
+        /run/current-system/sw/bin/stow -R . || { echo "Failed to stow ."; exit 1; }
       '';
 
       # System Settings for macOS
@@ -248,13 +248,125 @@
       };
     };
 
+    macosConfigModule_work = { pkgs, config, ... }: {
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in pkgs.lib.mkForce ''
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+      '';
+        system.activationScripts.script.text = ''
+          echo "Stowing dotfiles..."
+          cd /Users/jrollet@perfectserve.net/nix-darwin || { echo "Failed to cd into ~/nix-darwin/dotfiles"; exit 1; }
+          echo "Stowing $dir..."
+          /run/current-system/sw/bin/stow -R . || { echo "Failed to stow ."; exit 1; }
+        '';
+      # System Settings for macOS
+      # Documentation at: mynixos.com and look for nix-services
+      system.defaults = {
+        dock.autohide = false;
+        dock.tilesize = 24;
+        dock.largesize = 64;
+        dock.mineffect = "genie";
+        dock.mru-spaces = false;
+        dock.showhidden = true;
+        dock.launchanim = true;
+        dock.orientation = "bottom";
+        dock.static-only = false;
+        dock.show-recents = false;
+        dock.magnification = true;
+        # dock.autohide-delay = 0.05;
+        # dock.autohide-time-modifier = 0.05;
+        # Apps installed via nix package must include ${pkgs.APPNAME}
+        dock.persistent-apps = [
+          "/Applications/Alacritty.app"
+          "/Applications/GoLand.app"
+          "/Applications/Arc.app"
+          "/Applications/Slack.app"
+          "/Applications/Outlook.app"
+          "/Applications/Passwords.app"
+          "/Applications/Sublime.app"
+          "/Applications/OpenConnect-GUI.app"
+          "/Applications/Remote Desktop Manager Free.app"
+        ];
+        dock.persistent-others =
+                [
+                  "/Applications"
+                ];
+        dock.wvous-bl-corner = 1;
+        dock.wvous-br-corner = 1;
+        dock.wvous-tl-corner = 2;
+        dock.wvous-tr-corner = 1;
+        dock.slow-motion-allowed = false;
+        dock.dashboard-in-overlay = true;
+        dock.expose-group-by-app = false;
+        dock.expose-animation-duration = 0.05;
+        dock.minimize-to-application = false;
+        menuExtraClock.IsAnalog = false;
+        menuExtraClock.ShowAMPM = false;
+        menuExtraClock.ShowDate = 0;
+        menuExtraClock.Show24Hour = false;
+        menuExtraClock.ShowSeconds = false;
+        menuExtraClock.ShowDayOfWeek = false;
+        finder.ShowPathbar = true;
+        finder.QuitMenuItem = false;
+        finder.CreateDesktop = false;
+        finder.ShowStatusBar = true;
+        finder.AppleShowAllFiles = true;
+        finder.FXPreferredViewStyle = "clmv";
+        finder._FXSortFoldersFirst = true;
+        finder.AppleShowAllExtensions = true;
+        finder._FXShowPosixPathInTitle = true;
+        finder.FXDefaultSearchScope = "SCcf";
+        finder.FXEnableExtensionChangeWarning = false;
+        loginwindow.GuestEnabled = false;
+        trackpad.TrackpadThreeFingerDrag = true;
+        trackpad.TrackpadThreeFingerTapGesture = 0;
+        WindowManager.AutoHide = true;
+        WindowManager.StandardHideDesktopIcons = true;
+        WindowManager.HideDesktop = true;
+        WindowManager.EnableStandardClickToShowDesktop = false;
+        WindowManager.GloballyEnabled = false;
+        WindowManager.AppWindowGroupingBehavior = false;
+        NSGlobalDomain.AppleInterfaceStyle = "Dark";
+        NSGlobalDomain.KeyRepeat = 2;
+        NSGlobalDomain.InitialKeyRepeat = 10;
+        NSGlobalDomain.AppleShowAllFiles = true;
+        NSGlobalDomain.NSWindowResizeTime = 0.05;
+        NSGlobalDomain.AppleShowAllExtensions = true;
+        NSGlobalDomain.ApplePressAndHoldEnabled = false;
+        NSGlobalDomain.NSScrollAnimationEnabled = true;
+        NSGlobalDomain.NSDocumentSaveNewDocumentsToCloud = true;
+        NSGlobalDomain.NSAutomaticInlinePredictionEnabled = false;
+        NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+        NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false;
+        NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
+        NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false;
+        NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false;
+        NSGlobalDomain.NSWindowShouldDragOnGesture = true;
+        NSGlobalDomain.AppleScrollerPagingBehavior = true;
+        NSGlobalDomain."com.apple.keyboard.fnState" = true;
+        universalaccess.mouseDriverCursorSize = 1.25;
+      };
+    };
   in {
     darwinConfigurations."macos_personal" = nix-darwin.lib.darwinSystem {
       modules = [
         globalConfigModule
         globalPackagesModule
         macosPackagesModule
-        macosConfigModule
+        macosConfigModule_personal
         nix-homebrew.darwinModules.nix-homebrew
         ./services/yabai.nix
         ./services/skhd.nix
@@ -266,7 +378,7 @@
         globalConfigModule
         globalPackagesModule
         macosPackagesModule
-        macosConfigModule
+        macosConfigModule_work
         nix-homebrew.darwinModules.nix-homebrew
         ./services/yabai.nix
         ./services/skhd.nix
