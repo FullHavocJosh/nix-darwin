49cb929d776cfbd3804a535837e094bfdf76b64f
Josh <2626697+FullHavocJosh@users.noreply.github.com>
2024-12-03 07:40:30 -0500

    Update keymaps.lua
    Manually adopting changes that I forgot to commit last week.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -52,3 +52,8 @@ keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git 
 keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
 keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
 keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+
+-- bufferline
+keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true }) -- Go to next buffer
+keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true }) -- Go to previous buffer
+keymap.set("n", "<leader>bc", ":bdelete<CR>", { noremap = true, silent = true }) -- Close current buffers
