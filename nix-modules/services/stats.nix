8078547ff37bd5e30ed01da0a7065fe469c5df93
JRollet <josh@rollet.family>
2024-12-13 10:18:32 -0500

    disabling stats testing

--- nix-modules/services/stats.nix
+++ nix-modules/services/stats.nix
@@ -1,11 +1,11 @@
-{
-  launchd.jobs.stats = {
-    enable = true;
-    program = "/Applications/Stats.app/Contents/MacOS/Stats";
-    runAtLoad = true;
-    keepAlive = true;
-    environment = {
-      PATH = "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin";
-    };
-  };
-}
+#{
+#  launchd.jobs.stats = {
+#    enable = true;
+#    program = "/Applications/Stats.app/Contents/MacOS/Stats";
+#    runAtLoad = true;
+#    keepAlive = true;
+#    environment = {
+#      PATH = "/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin";
+#    };
+#  };
+#}
