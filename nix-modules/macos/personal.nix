b918f965d28030883e5bdb743b1ea599fde4976c
havoc <josh@rollet.family>
2025-11-18 17:04:10 -0500

    alacritty.

--- nix-modules/macos/personal.nix
+++ nix-modules/macos/personal.nix
@@ -19,7 +19,7 @@
   # Documentation at: mynixos.com and look for nix-services
   system.defaults = {
     dock.persistent-apps = [
-      "/Applications/Ghostty.app"
+      "/Applications/Alacritty.app"
       "/Applications/Zen.app"
       "/System/Applications/Music.app"
       "/System/Applications/Notes.app"
