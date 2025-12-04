cf3d8e1bd06af9497c9a13243a0a7254da386c43
fullhavocjosh <josh@rollet.family>
2025-12-04 13:17:29 -0500

    Fixing up omarchy config, 4K@13in is SMOL, needed larger elements for my aging eyes

--- .config/nvim.backup-20251201-081814/lua/plugins/tcomment_vim.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"tomtom/tcomment_vim",
-		config = function()
-			vim.keymap.set("n", "<leader>/", ":TComment<CR>", { desc = "Toggle comment for current line" })
-			vim.keymap.set("v", "<leader>/", ":TComment<CR>", { desc = "Toggle comment for selected lines" })
-		end,
-	},
-}
--- .config/nvim.backup-20251201-081814/plugin/fix-cmp-luasnip.lua
+++
@@ -1,6 +0,0 @@
--- Fix for cmp_luasnip after/plugin loading before nvim-cmp
--- This prevents the after/plugin file from loading during startup
--- We manually register it in nvim-cmp's config instead
-
--- Disable the after/plugin for cmp_luasnip
-vim.g.loaded_cmp_luasnip = 1
